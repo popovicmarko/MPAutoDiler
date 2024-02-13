6d3d38abee44137f51dc0da0f49344cc69ec63df
Marko <popovicmarko350@gmail.com>
2024-02-13 21:13:39 +0100

    Login page finished

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -1,19 +1,53 @@
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import ScrollToTop from "../ScrollToTop/ScrollToTop";
+import React, { useState } from 'react';
+import './login.css';
 
-import "./login.css";
+const LoginForm = () => {
+  const [korisničkoIme, postaviKorisničkoIme] = useState('');
+  const [lozinka, postaviLozinku] = useState('');
 
-function About() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Ovdje možete dodati logiku za slanje podataka na server ili autentikaciju
+    console.log('Korisničko ime:', korisničkoIme);
+    console.log('Lozinka:', lozinka);
+  };
+
   return (
-    <Container fluid className="about-section">
-      <Container>
-       
+    <div className='login'>
+    <div className="login-container">
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="korisnicko-ime">Username:</label>
+          <input
+            type="text"
+            id="korisnicko-ime"
+            value={korisničkoIme}
+            onChange={(e) => postaviKorisničkoIme(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="lozinka">Password:</label>
+          <input
+            type="password"
+            id="lozinka"
+            value={lozinka}
+            onChange={(e) => postaviLozinku(e.target.value)}
+          />
+        </div>
+        <button type="submit">Prijavi se</button>
+      </form>
+    </div>
+    </div>
+  );
+};
 
-      </Container>
-      <ScrollToTop />
-    </Container>
+function App() {
+  return (
+    <div className="App">
+      <LoginForm />
+    </div>
   );
 }
 
-export default About;
+export default App;
