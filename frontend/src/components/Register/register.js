a69b00bec1a3305a8581ca39cfc0cc0777023e97
Marko <popovicmarko350@gmail.com>
2024-02-13 23:37:10 +0100

    creted  regioster form

--- frontend/src/components/Register/register.js
+++ frontend/src/components/Register/register.js
@@ -1,19 +1,57 @@
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import ScrollToTop from "../ScrollToTop/ScrollToTop";
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './register.css'
 
+const RegistrationForm = () => {
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+  });
 
-import "./register.css";
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
 
-function Projects() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Ovdje možete dodati logiku za slanje podataka na server
+    console.log(formData);
+  };
+
   return (
-    <Container fluid className="project-section">
-      <Container>
-        
-      </Container>
-      <ScrollToTop />
-    </Container>
+    <div className="container-reg">
+      <div className="row justify-content-center ">
+        <div className="col-md-6" >
+          <h2>Registracija</h2>
+          <form onSubmit={handleSubmit}>
+            <div className="mb-3">
+              <label htmlFor="username" className="form-label">Korisničko ime</label>
+              <input type="text" className="form-control" id="username" name="username" value={formData.username} onChange={handleChange} />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">Email adresa</label>
+              <input type="email" className="form-control" id="email" name="email" value={formData.email} onChange={handleChange} />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="password" className="form-label">Lozinka</label>
+              <input type="password" className="form-control" id="password" name="password" value={formData.password} onChange={handleChange} />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="confirmPassword" className="form-label">Potvrdite lozinku</label>
+              <input type="password" className="form-control" id="confirmPassword" name="confirmPassword" value={formData.confirmPassword} onChange={handleChange} />
+            </div>
+            <button type="submit" className="btn btn-primary">Registracija</button>
+          </form>
+        </div>
+      </div>
+    </div>
   );
-}
+};
 
-export default Projects;
+export default RegistrationForm;
